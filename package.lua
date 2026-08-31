cc34d4416a215e826a2fbebf4e6225cd9f52c29a
filispeen <illayfilisp@gmail.com>
2026-08-31 20:32:43 +0300

    version replace in ci

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
   name = "filispeen/lavalink.lua",
-  version = "0.2.7",
+  version = "REPLACE_ME",
   description = "Feature-rich Lavalink v4 client for Luvit (Lua)",
   tags = { "lavalink", "discord", "music", "audio", "luvit" },
   license = "MIT",
